32bfb4962dee02d2db06c9191e9af0c3185e96e8
Felice Serena <felice@serena-mueller.ch>
2016-01-24 11:39:55 +0000

    Initializing project

---
+++ CPU Spy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CPU Spy
+//
+//  Created by Felice Serena on 24.01.16.
+//  Copyright © 2016 Serena. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
